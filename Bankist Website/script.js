0d88e7e5b72ca0a67045f95a35329b8057c2aed0
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-12-22 19:17:43 +0100

    Update script.js

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -120,3 +120,25 @@ const handleHover = function (e, opacity) {
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 ///////////////////////
+//reveal sections
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
